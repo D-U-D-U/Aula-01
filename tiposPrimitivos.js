c62a34fac66b0292039e86c5c66ccc17f28136df
D-U-D-U <claudioecbarros@gmail.com>
2024-09-17 00:04:44 +0000

    atualizando código 01

---
+++ tiposPrimitivos.js
@@ -0,0 +1,8 @@
+let rotulo = "Natalya" //declaramos a variavel
+rotulo = 12345 //reatribuimos o tipo de dado
+console.log(rotulo);
+
+const num1 = 10
+const num2 = 20
+const calc = num1 + num2
+console.log("O resultado é " + calc)
